6a399b14e0a93c5713f60339e94126f11678d1a7
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-04-19 02:19:41 +0530

    Button Input

--- Blog/src/components/Header/LogoutBtn.jsx
+++ Blog/src/components/Header/LogoutBtn.jsx
@@ -1,7 +1,24 @@
-const Logout = () => {
+import {useDispatch} from 'react-redux'
+import authService from '../../appwrite/config'
+import {logout} from '../../store/authSlice'
+
+const LogoutBtn = () => {
+
+  const dispatch = useDispatch() ;
+  const logoutHandler = ()=>{
+    authService.logout()
+    .then(()=>{
+      dispatch(logout) 
+    })
+    .catch((error)=>{
+      console.log("logout failed :: error from :: LOGOUTBTN.JSX :: 12",error)
+    })
+  }
+
   return (
-    <div>Logout</div>
+    <button className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    onClick={logoutHandler}>Logout</button>
   )
 }
 
-export default Logout
+export default LogoutBtn
