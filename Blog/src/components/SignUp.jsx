0905f9b650f380b2c8ecbfdda3eee9b4a919f2d8
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-04-20 12:18:50 +0530

    SingUp component created and learned about react hook form

--- Blog/src/components/SignUp.jsx
+++ Blog/src/components/SignUp.jsx
@@ -1,26 +1,28 @@
 import { useState } from "react";
-import { Link, Navigate } from "react-router-dom";
-import { login as authLogin } from "../store/authSlice";
-import { Input, Button } from "./index";
-import { useDispatch } from "react-redux";
+
 import authService from "../appwrite/auth";
+import { Link, Navigate } from "react-router-dom";
+import { login } from "../store/store";
+import { Button, Input } from "./index";
+import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 
-const Login = () => {
-  const dispatch = useDispatch();
-  const navigate = Navigate();
-
-  const { register, handleSubmit } = useForm();
+const SignUp = () => {
   const [error, setError] = useState("");
+  const navigate = Navigate();
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
 
-  const login = async (data) => {
+  const create = async () => {
     setError("");
     try {
-      const session = await authService.login(data);
-      if (session) {
+      const userData = await authService.createAccount(data);
+      if (userData) {
         const userData = await authService.getCurrentUser();
-        if (userData) dispatch(authLogin);
-        navigate("/");
+        if (userData) {
+          dispatch(login(userData));
+          navigate("/");
+        }
       }
     } catch (error) {
       setError(error.message);
@@ -28,7 +30,7 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="flex items-center justify-center">
       <div
         className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
       >
@@ -38,24 +40,31 @@ const Login = () => {
           </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight">
-          Sign in to your account
+          Sign up to create account
         </h2>
         <p className="mt-2 text-center text-base text-black/60">
-          Don&apos;t have any account?&nbsp;
+          Already have an account?&nbsp;
           <Link
-            to="/signup"
+            to="/login"
             className="font-medium text-primary transition-all duration-200 hover:underline"
           >
-            Sign Up
+            Sign In
           </Link>
         </p>
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
 
-        <form onSubmit={handleSubmit(login)} className="mt-8">
+        <form onSubmit={handleSubmit(create)}>
           <div className="space-y-5">
             <Input
-              label="email"
-              placeholder="Enter your Email"
+              label="Full Name: "
+              placeholder="Enter your full name"
+              {...register("name", {
+                required: true,
+              })}
+            />
+            <Input
+              label="Email: "
+              placeholder="Enter your email"
               type="email"
               {...register("email", {
                 required: true,
@@ -66,18 +75,16 @@ const Login = () => {
                 },
               })}
             />
-
             <Input
-              label="password"
+              label="Password: "
               type="password"
-              placeholder="Enter Your Password"
+              placeholder="Enter your password"
               {...register("password", {
                 required: true,
               })}
             />
-
-            <Button type="submit" className="2-full">
-              Sign In
+            <Button type="submit" className="w-full">
+              Create Account
             </Button>
           </div>
         </form>
@@ -86,4 +93,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
