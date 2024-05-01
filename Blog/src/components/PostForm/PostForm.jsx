a781db80e2d12ac897f702d67dcbce2cab9ad273
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-05-01 12:55:55 +0530

    Post Form Updated

--- Blog/src/components/PostForm/PostForm.jsx
+++ Blog/src/components/PostForm/PostForm.jsx
@@ -19,7 +19,31 @@ export const PostForm = ({post}) => {
   const navigate = useNavigate() ;
   const userData = useSelector(state =>state.userData) ;
 
-  
+  const submit = async(data) =>{
+    if(post){
+      const file = data.image[0]?appwriteService.uploadFile(data.image[0]) :null ;
+      +
+      if(file){
+        appwriteService.deleteFile(post.featuredImage) 
+      }
+
+      const dbPost = await appwriteService.updatePost(
+        post.$id,
+        {
+          ...data,
+          featuredImage : file ? file.$id : undefined
+        }
+      )
+
+      if(dbPost){
+        navigate(`/post/${dbPost.$id}`) 
+      }
+
+    }else{
+      const file = await appwriteService.uploadFile(data.image[0])
+    }
+  }
+
   return (
     <div>PostForm</div>
   )
